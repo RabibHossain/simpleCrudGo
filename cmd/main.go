9641b9eca6660b0281afe57509616943d4899562
RabibHossain <galib.rabib@gmail.com>
2021-09-13 02:23:31 +0600

    chiRequests

--- cmd/main.go
+++ cmd/main.go
@@ -2,22 +2,56 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
+	"net/http"
 	"newsfeed/platform/newsfeed"
 
+	"github.com/go-chi/chi/v5"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/qkgo/yin"
 )
 
 func main() {
 	db, _ := sql.Open("sqlite3", "./newsfeed.db")
 	feed := newsfeed.NewFeed(db)
 
-	// feed.Add(newsfeed.Item{
-	// 	Content: "Hello!",
-	// })
+	// // feed.Add(newsfeed.Item{
+	// // 	Content: "Hello!",
+	// // })
 
-	items := feed.Get()
+	// items := feed.Get()
 
-	fmt.Println(items)
+	// fmt.Println(items)
+
+	r := chi.NewRouter()
+	r.Use(yin.SimpleLogger)
+
+	r.Get("/posts", func(w http.ResponseWriter, r *http.Request) {
+		res, _ := yin.Event(w, r)
+		items := feed.Get()
+		res.SendJSON(items)
+	})
+
+	r.Post("/posts", func(w http.ResponseWriter, r *http.Request) {
+		res, req := yin.Event(w, r)
+		body := map[string]string{}
+		req.BindBody(&body)
+		item := newsfeed.Item{
+			Content: body["content"],
+		}
+		feed.Add(item)
+		res.SendJSON(204)
+	})
+
+	http.ListenAndServe(":3000", r)
 
 }
+
+// curl -i -X POST -H 'Content-Type: application/json' -d '{"content": "New item"}' http://localhost:3000/posts
+
+// fetch('/posts', {
+// 	method: 'POST',
+// 	headers: {'content-type': 'application/json'},
+// 	body: JSON.stringify({
+// 		content: 'Hola'
+// 	})
+// })
--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module newsfeed
 
 go 1.17
 
-require github.com/mattn/go-sqlite3 v1.14.8 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.0.4 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-sqlite3 v1.14.8 // indirect
+	github.com/newrelic/go-agent v3.15.0+incompatible // indirect
+	github.com/qkgo/yin v0.0.0-20190403073632-bff3b50ba6e6 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+)
